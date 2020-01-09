1a50e36d45d9ba344fb6438cc4be999315ae10be
nishiura hiroki <nishiurahiroki@gmail.com>
2020-01-09 10:18:50 -0500

    develop todo update.

--- src/App.elm
+++ src/App.elm
@@ -9,41 +9,74 @@ import List.Extra
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
+type alias Todo =
+  {
+    content : String,
+    isUpdateMode : Bool
+  }
+
 type alias Model =
   {
-    todo : String,
-    todoList : List String
+    inputedTodo : String,
+    inputedUpdateTodo : String,
+    todoList : List Todo
   }
 
 init : Model
 init =
   {
-    todo = "",
+    inputedTodo = "",
+    inputedUpdateTodo = "",
     todoList = []
   }
 
 type Msg =
   InputTodo String |
+  InputUpdateTodo Int String |
   AddTodo |
-  DeleteTodo Int
+  DeleteTodo Int |
+  TransFormInputText Int |
+  UpdateTodo Int String
+
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     InputTodo inputedTodo ->
       {
-        model | todo = inputedTodo
+        model | inputedTodo = inputedTodo
+      }
+    InputUpdateTodo updateIndex inputedUpdateTodo ->
+      {
+        model |
+          todoList = List.Extra.updateAt updateIndex (\todo -> { todo | content = inputedUpdateTodo }) model.todoList
       }
     AddTodo ->
       {
         model |
-          todoList = model.todo :: model.todoList,
-          todo = ""
+          todoList = {
+            content = model.inputedTodo,
+            isUpdateMode = False
+          } :: model.todoList,
+          inputedTodo = ""
       }
     DeleteTodo deleteIndex ->
       {
         model |
           todoList = List.Extra.removeAt deleteIndex model.todoList
+      }
+    TransFormInputText transFormIndex ->
+      {
+        model |
+          todoList =
+            List.map (\todo -> { todo | isUpdateMode = False }) model.todoList
+              |> List.Extra.updateAt transFormIndex (\todo -> {todo | isUpdateMode = True})
+      }
+    UpdateTodo updateIndex content ->
+      {
+        model |
+          todoList =
+            List.Extra.updateAt updateIndex (\todo -> { todo | content = content, isUpdateMode = False }) model.todoList
       }
 
 
@@ -52,16 +85,23 @@ view model =
   div []
     [
       p [] [
-        input [ type_ "text", value model.todo , onInput InputTodo ] [],
+        input [ type_ "text", value model.inputedTodo , onInput InputTodo ] [],
         text " : TODO ",
         button [ onClick AddTodo ] [ text "追加" ]
       ],
       p [] (List.indexedMap viewTodo model.todoList)
     ]
 
-viewTodo : Int -> String -> Html Msg
+
+viewTodo : Int -> Todo -> Html Msg
 viewTodo index todo =
-  p [] [
-    text todo,
-    button [ onClick (DeleteTodo index) ] [text "削除"]
-  ]
+  if todo.isUpdateMode then
+    p [] [
+      input  [ type_ "text", value todo.content, onInput (InputUpdateTodo index) ] [ ],
+      button [ onClick (UpdateTodo index todo.content) ] [ text "更新" ] -- TODO ↑のテキストが変更された時点でtodo.contentがupdateされてるからここでも更新してる意味あんまない?
+    ]
+  else
+    p [] [
+      span   [ onDoubleClick (TransFormInputText index) ] [ text todo.content ],
+      button [ onClick (DeleteTodo index) ] [text "削除"]
+    ]
